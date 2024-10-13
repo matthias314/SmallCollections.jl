2e47a72a3427890218abd18dbe8eb4295baee1c6
matthias314 <matthias314@posteo.net>
2024-10-13 19:59:28 -0400

    added docstrings

--- src/fixedvector.jl
+++ src/fixedvector.jl
@@ -8,13 +8,63 @@ import Base: Tuple, ==, isequal, size,
     mapfoldl, mapfoldr, vcat, copy, copyto!, convert,
     strides, elsize, unsafe_convert, muladd
 
+"""
+    AbstractFixedVector{N,T} <: AbstractVector{T}
+
+`AbstractFixedVector{N,T}` is the supertype of `FixedVector{N,T}` and `MutableFixedVector{N,T}`.
+
+See also [`FixedVector{N,T}`](@ref), [`MutableFixedVector{N,T}`](@ref).
+"""
 abstract type AbstractFixedVector{N,T} <: AbstractVector{T} end
 
+"""
+    FixedVector{N,T} <: AbstractFixedVector{N,T}
+
+    FixedVector{N,T}(iter)
+    FixedVector{N}(iter)
+    FixedVector(iter)
+
+`FixedVector{N,T}` is an immutable vector type that holds exactly `N` elements of type `T`.
+(It is analogous to `StaticArrays.SVector` and `StaticVectors.Values`.)
+The size `N` can be any (small) positive integer. However, at least for bit integer
+and hardware float types, one usually takes `N` to be a power of `2`.
+
+If the element type `T` or the size `N` are omitted, they are determined from the iterator
+given as argument. Performance degrades if this is not possible at compile time.
+As a rule of thumb, the size should only be omitted for `Tuple` arguments.
+
+See also [`MutableFixedVector{N,T}`](@ref).
+"""
 struct FixedVector{N,T} <: AbstractFixedVector{N,T}
     t::NTuple{N,T}
     FixedVector{N,T}(t::NTuple{N,Any}) where {N,T} = new{N,T}(t)
 end
 
+"""
+    MutableFixedVector{N,T} <: AbstractFixedVector{N,T}
+
+    MutableFixedVector{N,T}(iter)
+    MutableFixedVector{N}(iter)
+    MutableFixedVector(iter)
+
+    MutableFixedVector{N,T}(undef)
+
+`MutableFixedVector{N,T}` is a mutable vector type that holds exactly `N` elements of type `T`.
+(It is analogous to `StaticArrays.MVector` and `StaticVectors.Variables`.)
+The size `N` can be any (small) positive integer. However, at least for bit integer
+and hardware float types, one usually takes `N` to be a power of `2`.
+
+If the element type `T` or the size `N` are omitted, they are determined from the iterator
+given as argument. Performance degrades if this is not possible at compile time.
+As a rule of thumb, the size should only be omitted for `Tuple` arguments.
+
+Note that setting individual vector elements (via `setindex!`) is only supported for `isbits`
+element types.
+
+The special form `MutableFixedVector{N,T}(undef)` returns a non-initialized vector.
+
+See also [`FixedVector{N,T}`](@ref), `Base.isbits`.
+"""
 mutable struct MutableFixedVector{N,T} <: AbstractFixedVector{N,T}
     t::NTuple{N,T}
     MutableFixedVector{N,T}(t::NTuple{N,Any}) where {N,T} = new{N,T}(t)
