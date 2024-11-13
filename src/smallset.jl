d49a120df5ce3be24ab3219a4152d2a29d4f74d1
matthias314 <matthias314@posteo.net>
2024-11-13 13:02:48 -0500

    added `Base.copymutable` for `AbstractSmallSet`

--- src/smallset.jl
+++ src/smallset.jl
@@ -151,6 +151,7 @@ empty(::AbstractSmallSet, ::Type)
 empty(s::SmallSet{N,T}, ::Type{U} = T) where {N,T,U} = SmallSet{N,U}()
 empty(s::MutableSmallSet{N,T}, ::Type{U} = T) where {N,T,U} = MutableSmallSet{N,U}()
 
+Base.copymutable(s::AbstractSmallSet) = MutableSmallSet(s)
 Base.emptymutable(s::AbstractSmallSet{N,T}, ::Type{U} = T) where {N,T,U} = MutableSmallSet{N,U}()
 
 """
