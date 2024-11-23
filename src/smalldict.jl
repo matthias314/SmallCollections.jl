cf957a7b9fcdecab0b1e098dfa0cc64556071ee9
matthias314 <matthias314@posteo.net>
2024-11-23 17:20:53 -0500

    fixed docstring for `pop` for `AbstractSmallDict`

--- src/smalldict.jl
+++ src/smalldict.jl
@@ -270,9 +270,9 @@ function delete(d::AbstractSmallDict, key)
 end
 
 """
-    pop(d::AbstractSmallDict{N,K,V}) where {N,K,V} -> Tuple{SmallDict{N,K,V}, V}
+    pop(d::AbstractSmallDict{N,K,V}) where {N,K,V} -> Tuple{SmallDict{N,K,V}, Pair{K,V}}
 
-Remove a mapping `key => val` from `d` and return the new dictionary together with `val`.
+Remove a mapping `key => val` from `d` and return the new dictionary together with the mapping.
 The dictionary `d` must not be empty.
 
 See also `Base.pop!`.
