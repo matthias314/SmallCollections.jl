b9979fd6bf3a2df962576b0ea7d3a4f9a1943bf2
matthias314 <matthias314@posteo.net>
2025-05-06 16:00:29 -0400

    added documentation for v0.4.0

--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.4.0",
   "v0.3.0",
   "v0.2.0",
   "v0.1.2",
@@ -7,5 +8,5 @@ var DOC_VERSIONS = [
   "v0.1.0",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.3.0";
+var DOCUMENTER_NEWEST = "v0.4.0";
 var DOCUMENTER_STABLE = "stable";
