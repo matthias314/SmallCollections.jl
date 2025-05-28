da174b3be4e79efeeb96f1c58e7b65ea77ae581a
matthias314 <matthias314@posteo.net>
2025-05-28 08:54:30 -0400

    added `partitions(n, k)`

--- src/combinatorics.jl
+++ src/combinatorics.jl
@@ -38,6 +38,8 @@ Return an iterator over the partitions of `n`.
 A partition of `n` is a weakly decreasing sequence of positive integers that add up to `n`.
 Each partition is of type `SmallVector{$PartN,$PartEltype}`, but this may change in the future.
 
+See also [`partitions(::Integer, ::Integer)`](@ref).
+
 # Examples
 ```jldoctest
 julia> partitions(3) |> collect
@@ -95,6 +97,70 @@ end
             SmallVector(v), (v, length(v)-isone(s), PartEltype(isone(s)))
         end
     end
+end
+
+struct Partitions2
+    n::Int
+    k::Int
+end
+
+"""
+    partitions(n::Integer, k::Integer)
+
+Return an iterator over the partitions of `n` into `k` parts.
+A partition of `n` is a weakly decreasing sequence of positive integers that add up to `n`.
+Each partition is of type `SmallVector{$PartN,$PartEltype}`, but this may change in the future.
+
+See also [`partitions(::Integer)`](@ref).
+
+# Examples
+```jldoctest
+julia> partitions(7, 3) |> collect
+4-element Vector{SmallVector{64, Int8}}:
+ [5, 1, 1]
+ [4, 2, 1]
+ [3, 3, 1]
+ [3, 2, 2]
+
+julia> partitions(7, 0) |> collect
+SmallVector{64, Int8}[]
+
+julia> partitions(0, 0) |> collect
+1-element Vector{SmallVector{64, Int8}}:
+ 0-element SmallVector{64, Int8}
+```
+"""
+function partitions(n::Integer, k::Integer)
+    (n >= 0 && k >= 0) || error("arguments must be non-negative")
+    n <= typemax(PartEltype) || error("only partitions of integers <= $(typemax(PartEltype)) are supported")
+    k <= PartN || error("only partitions into at most $PartN parts are supported")
+    Partitions2(n, k)
+end
+
+IteratorSize(::Type{Partitions2}) = Base.SizeUnknown()
+
+eltype(::Type{Partitions2}) = SmallVector{PartN,PartEltype}
+
+@inline function iterate(p::Partitions2)
+    (p.n < p.k || p.n > p.k == 0) && return nothing
+    v = MutableSmallVector{PartN,PartEltype}(undef, p.k)
+    for i in eachindex(v)
+        @inbounds v[i] = (i == 1 ? p.n-p.k+1 : 1) % PartEltype
+    end
+    SmallVector(v), v
+end
+
+@inline function iterate(p::Partitions2, v)
+    @inbounds begin
+        c = v[1] - one(PartEltype)
+        c <= v[p.k] && return nothing
+        i = findfirst(<(c), v)::Int
+        c = (v[i] += one(PartEltype))
+        # unsafe_copyto!(v, map(Fix2(min, c), v; style = RigidStyle()))  # allocates
+        v.b = min.(v.b, c)
+        v[1] += p.n % PartEltype - sum_fast(v)
+    end
+    SmallVector(v), v
 end
 
 #
--- test/combinatorics.jl
+++ test/combinatorics.jl
@@ -19,6 +19,20 @@ unsigned_types = (UInt8, UInt64, UInt256, UInt440)
         n == 0 && @test length(v) == 1
         n == 8 && @test length(v) == 22
     end
+
+    for n in [-1, 0, 20, typemax(PartEltype)+1], k in [-1, 0, 1, 10, 20, PartN+1]
+        if !(0 <= n <= typemax(PartEltype) && 0 <= k <= PartN)
+            @test_throws Exception partitions(n, k)
+            continue
+        end
+        @test_inferred eltype(typeof(partitions(n, k))) SmallVector{PartN,PartEltype}
+        v = @inferred collect(partitions(n, k))
+        @test all(p -> issorted(p; rev = true) && all(>(0), p), v)
+        @test all(p -> length(p) == k && sum(p; init = 0) == n, v)
+        @test allunique(v)
+        p20 = Dict(0 => 0, 1 => 1, 10 => 42, 20 => 1)
+        n == 20 && @test length(v) == p20[k]
+    end
 end
 
 @testset "compositions" begin
