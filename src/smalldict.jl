4c69b0ff055f495c66f2057bf8e117c6d1f13f7a
matthias314 <matthias314@posteo.net>
2026-01-09 21:23:24 -0500

    fixed docstrings for `empty` for `AbstractSmallDict`, `AbstractSmallSet`

--- src/smalldict.jl
+++ src/smalldict.jl
@@ -295,18 +295,17 @@ See also `Base.setindex!`, [`push`](@ref push(::AbstractSmallDict, ::Pair)).
 end
 
 """
-    empty(d::AbstractSmallDict{N,K,V}) where {N,K,V,W} -> AbstractSmallVector{N,K,V}
-    empty(d::AbstractSmallDict{N,K,V}, W::Type) where {N,K,V,W} -> AbstractSmallVector{N,K,W}
-    empty(d::AbstractSmallDict{N,K,V}, L::Type, W::Type) where {N,K,V,L,W} -> AbstractSmallVector{N,L,W}
+    empty(d::AbstractSmallDict{N,K,V}) where {N,K,V} -> AbstractSmallDict{N,K,V}
+    empty(d::AbstractSmallDict{N,K,V}, W::Type) where {N,K,V,W} -> AbstractSmallDict{N,K,W}
+    empty(d::AbstractSmallDict{N,K,V}, L::Type, W::Type) where {N,K,V,L,W} -> AbstractSmallDict{N,L,W}
 
-Return an empty `AbstractSmallDictionary` with the same capacity as `d`,
+Return an empty `AbstractSmallDict` with the same capacity as `d`,
 and with `valtype` changed to `W` and `keytype` changed to `L` if so specified.
 The resulting dictionary is mutable if and only if `d` is so.
 """
 empty(::AbstractSmallDict),
 empty(::AbstractSmallDict, ::Type),
 empty(::AbstractSmallDict, ::Type, ::Type)
-
 
 empty(d::SmallDict{N,K,V}, ::Type{W} = V) where {N,K,V,W} = SmallDict{N,K,W}()
 empty(d::MutableSmallDict{N,K,V}, ::Type{W} = V) where {N,K,V,W} = MutableSmallDict{N,K,W}()
--- src/smallset.jl
+++ src/smallset.jl
@@ -149,13 +149,12 @@ Base.hasfastin(::Type{S}) where S <: AbstractSmallSet = Base.hasfastin(fieldtype
 in(x, s::AbstractSmallSet) = haskey(s.d, x)
 
 """
-    empty(d::AbstractSmallDict{N,K,V}) where {N,K,V,W} -> AbstractSmallVector{N,K,V}
-    empty(d::AbstractSmallDict{N,K,V}, W::Type) where {N,K,V,W} -> AbstractSmallVector{N,K,W}
-    empty(d::AbstractSmallDict{N,K,V}, L::Type, W::Type) where {N,K,V,L,W} -> AbstractSmallVector{N,L,W}
+    empty(s::AbstractSmallSet{N,T}) where {N,T} -> AbstractSmallSet{N,T}
+    empty(s::AbstractSmallSet{N,T}, U::Type) where {N,T,U} -> AbstractSmallSet{N,U}
 
-Return an empty `AbstractSmallDictionary` with the same capacity as `d`,
-and with `valtype` changed to `W` and `keytype` changed to `L` if so specified.
-The resulting dictionary is mutable if and only if `d` is so.
+Return an empty `AbstractSmallSet` with the same capacity as `s`,
+and with element type changed to `U` if so specified.
+The resulting dictionary is mutable if and only if `s` is so.
 """
 empty(::AbstractSmallSet),
 empty(::AbstractSmallSet, ::Type)
