06c1c97cf916133509cf05fa8e618aed86127d59
matthias314 <matthias314@posteo.net>
2026-08-07 10:12:15 -0400

    added docs for v0.6.3

--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.6.3",
   "v0.6.1",
   "v0.6.0",
   "v0.5.1",
@@ -13,5 +14,5 @@ var DOC_VERSIONS = [
   "v0.1.0",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.6.1";
+var DOCUMENTER_NEWEST = "v0.6.3";
 var DOCUMENTER_STABLE = "stable";
